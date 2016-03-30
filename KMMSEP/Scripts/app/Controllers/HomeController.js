9bfca63a378bacbcf0c50e08650ff084d3bf318f
GeorgeRibiy <ribiy49@gmail.com>
2016-03-30 17:58:16 +0300

    Please enter the commit message for your changes. Lines starting

---
+++ KMMSEP/Scripts/app/Controllers/HomeController.js
@@ -0,0 +1,6 @@
+﻿app.controller('HomeController', ['$scope', function ($scope) {
+    $('#main-slider .carousel').carousel({
+        interval: 8000
+    });
+    console.log($('#main-slider .carousel').html());
+}])
