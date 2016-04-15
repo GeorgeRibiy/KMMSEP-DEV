ad83f483f7ddae022b06268b761bbd451172ccf8
GeorgeRibiy <ribiy49@gmail.com>
2016-04-15 08:30:17 +0300

    workers subject agragation
    ...

--- KMMSEP/Scripts/app/Controllers/StuffController.js
+++ KMMSEP/Scripts/app/Controllers/StuffController.js
@@ -1,6 +1,26 @@
-﻿app.controller('StuffController', ['$scope', 'StuffService', function ($scope, StuffService) {
+﻿app.controller('StuffController', ['$scope', '$mdDialog', '$mdMedia', 'StuffService', function ($scope,$mdDialog, $mdMedia, StuffService) {
     var init = function () {
         $scope.workers = StuffService.getAll();
+    }
+
+    $scope.openSubjects = function (ev, index) {
+        var selfScope = $scope;
+        $mdDialog.show({
+            controller: function ($scope) {
+                $scope.subjects = selfScope.workers[index].subjects;
+                $scope.cancel = function () {
+                    $mdDialog.hide();
+                }
+            },
+            templateUrl: 'Views/Modals/SubjectModal.html',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose: true,
+            bindToController: true,
+            locals: {
+                advert: $scope.workers[index]
+            }
+        })
     }
 
     init();
